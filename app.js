6bc613dacccb629061e4de1a4b543e7d031fc332
Michal Drbohlav <m.drbohlav1@gmail.com>
2016-12-03 20:31:00 +0100

    init

---
+++ app.js
@@ -0,0 +1,135 @@
+var express = require('express'),
+    path = require('path'),
+    logger = require('morgan'),
+    moment = require('moment'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
+
+var stylus = require('stylus'),
+    nib = require('nib'),
+    fs = require('fs'),
+    uglify = require("uglify-js");
+
+var routes = require('./routes/index'),
+    users = require('./routes/users');
+
+var app = express();
+
+// Nastavení šablonovacího systému na Jade
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// ## Nastavení Stylusu
+// Funkce, která zajistí kompilaci Stylus souborů, jejich kompresi a
+// použití knihovny nib, která doplní vendor prefixy a obsahuje 
+// různé užitečné prefixy.
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .use(nib())
+        .import('nib');
+}
+
+// Nastavení middleware pro Stylus, který zavolá funkci na kompilaci.
+app.use(stylus.middleware({
+    src: path.join(__dirname, '/public'),
+    dest: path.join(__dirname, '/public'),
+    compile: compile,
+    debug: true
+}));
+
+// ## Nastavení minifikace JavaScriptu
+// Nejprve se spojí všechny používané JavaScriptové knihovny do jednoho
+// souboru, který se následně minifikuje s nastavenými parametry.
+// - `sequences` - spojení po sobě jdoucích jednoduchých výrazů čárkou.
+// - `dead_code` - odstranění nedosažitelného kódu.
+// - `conditionals` - optimalizace `if` podmínek.
+// - `booleans` - optimalizace boolean výrazů jako je `!!a ? b : c` na `a ? b : c`.
+// - `unused` - odstranění nepoužívaných funkcí a proměnných.
+// - `if_return` - optimalizace `if/return` a `if/continue` podmínek.
+// - `join_vars` - spojení po sobě jdoucích `var` definic.
+// - `drop_console` - odstranění výpisů do konzole.
+var uglified = uglify.minify([
+    path.join(__dirname, 'bower_components/jquery/dist/jquery.js'),
+    path.join(__dirname, 'bower_components/velocity/velocity.js'),
+    path.join(__dirname, 'bower_components/console-polyfill/index.js'),
+    path.join(__dirname, 'bower_components/es5-shim/es5-shim.js'),
+    path.join(__dirname, 'bower_components/fastclick/lib/fastclick.js')
+], {
+    mangle: true,
+    compress: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true
+    }
+});
+
+// Uložení minifikovaného souboru do `public/js/app.min.js`.
+fs.writeFile(path.join(__dirname, 'public/js/app.min.js'), uglified.code, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Script generated and saved.");
+    }
+});
+
+// ## Nastavení logování
+// Registrace vlastního formátu data pro logování.
+logger.token('customDate', function(req, res) {
+    return '[' + moment().format('gggg-MM-DD HH:mm:ss.SSS') + ']';
+});
+// Nastavení formátu logování.
+app.use(logger(':customDate - :method :url :status :response-time ms - :res[content-length]'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Zde ošetřit vstup na přehled výsledků
+app.get('/results*', function(req, res, next) {
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ public/js/global.js
@@ -0,0 +1,241 @@
+var $window = $(window),
+    $body = $('body');
+
+function pad(n) {
+    return n < 10 ? '0' + n : n;
+}
+
+function urlParam(name) {
+    var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results === null) {
+        return null;
+    } else {
+        return results[1] || 0;
+    }
+}
+
+function createCookie(name, value, expires) {
+    var cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; path=/";
+    if (typeof(expires) !== 'undefined') {
+        cookie += '; expires=' + new Date(expires).toUTCString();
+    }
+    document.cookie = cookie;
+}
+
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+function Timer(callback, delay) {
+    var timerId, start, remaining = delay;
+
+    this.pause = function() {
+        clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function() {
+        start = new Date();
+        clearTimeout(timerId);
+        timerId = setTimeout(callback, remaining);
+    };
+
+    this.clear = function() {
+        clearTimeout(timerId);
+    };
+
+    this.resume();
+}
+
+function setBodyOverflowHidden() {
+    var overflowHiddenCnt = typeof($body.attr('data-overflow-hidden')) !== 'undefined' ? parseInt($body.attr('data-overflow-hidden')) + 1 : 1;
+    $body.attr('data-overflow-hidden', overflowHiddenCnt);
+    if (overflowHiddenCnt === 1) {
+        $body.addClass('overflow--hidden');
+    }
+}
+
+function removeBodyOverflowHidden() {
+    var overflowHiddenCnt = parseInt($body.attr('data-overflow-hidden')) - 1;
+    $body.attr('data-overflow-hidden', overflowHiddenCnt);
+    if (overflowHiddenCnt === 0) {
+        $body.removeClass('overflow--hidden');
+    }
+}
+
+function getSpinner(white) {
+    var color = white ? ' spinner__white' : '';
+    return $('<div class="spinner' + color + '"><div class="spinner__child spinner__child__1"></div><div class="spinner__child spinner__child__2"></div><div class="spinner__child spinner__child__3"></div><div class="spinner__child spinner__child__4"></div><div class="spinner__child spinner__child__5"></div><div class="spinner__child spinner__child__6"></div><div class="spinner__child spinner__child__7"></div><div class="spinner__child spinner__child__8"></div><div class="spinner__child spinner__child__9"></div><div class="spinner__child spinner__child__10"></div><div class="spinner__child spinner__child__11"></div><div class="spinner__child spinner__child__12"></div></div>');
+}
+
+function getBtnSpinner() {
+    return $('<div class="btn__spinner"></div>').append(getSpinner(true));
+}
+
+function showModal() {
+    var $modal = $('.modal'),
+        $modalContent = $modal.find('.modal__content');
+
+    if (!$modal.is(':visible')) {
+
+        updateAfterHide = function() {
+            setBodyOverflowHidden();
+        };
+
+        $modal.velocity('fadeIn', {
+            complete: function() {
+                updateAfterHide();
+            }
+        });
+        $modalContent.velocity({
+            opacity: '1'
+        });
+    }
+}
+
+function hideModal() {
+    var $modal = $('.modal'),
+        $modalContent = $modal.find('.modal__content');
+
+    if ($modal.is(':visible')) {
+
+        updateAfterHide = function() {
+            removeBodyOverflowHidden();
+            $modalContent.removeAttr('style');
+        };
+
+        $modal.velocity('fadeOut', {
+            complete: function() {
+                updateAfterHide();
+            }
+        });
+    }
+}
+
+function makeRequest(url, method, data, cbSuccess, cbError) {
+    $.ajax({
+        url: url,
+        method: method,
+        data: data,
+        complete: function() {
+
+        },
+        success: function(res) {
+            if (typeof(cbSuccess) !== 'undefined') {
+                cbSuccess(res);
+            }
+        },
+        error: function(res) {
+            if (typeof(cbError) !== 'undefined') {
+                cbError(res);
+            }
+        }
+    });
+}
+
+function animateScale($element) {
+    $element.velocity({
+        scale: "0"
+    }, {
+        duration: 100,
+        complete: function() {
+            $element.toggleClass('active');
+        }
+    }).velocity({
+        scale: "1.2"
+    }, {
+        duration: 150
+    }).velocity({
+        scale: "1"
+    }, {
+        duration: 50
+    });
+}
+
+$(document).ready(function() {
+
+    //=================================================================
+    // Modal close
+    //=================================================================
+    $('.modal').on('click', function(event) {
+        var $this = $(this),
+            triggerClose = $('.modal, .modal__close, .modal__content').toArray();
+        if (triggerClose.indexOf(event.target) === -1) {
+            return;
+        }
+        hideModal();
+    });
+
+    //=================================================================
+    // Checkbox functionality
+    //=================================================================
+    $('input[type="checkbox"]').on('change', function() {
+        var $this = $(this),
+            $checkbox = $this.parent();
+        animateScale($checkbox);
+    });
+
+    //=================================================================
+    // Radio functionality
+    //=================================================================
+    $('input[type="radio"]').on('change', function() {
+        var $this = $(this),
+            $radio = $this.parent(),
+            group = $this.attr('name'),
+            $prev = $('input[name="' + group + '"][type="radio"]:not(:checked)');
+        if ($prev.length) {
+            $prev.parent().removeClass('active');
+        }
+        animateScale($radio);
+    });
+
+    //=================================================================
+    // Esc key listener - hide modal
+    //=================================================================
+    $(document).on('keyup', function(event) {
+        if (event.keyCode !== 27) {
+            return;
+        }
+        hideModal();
+    });
+
+    //=================================================================
+    // Scroll to id functionality
+    //=================================================================
+    $("a").click(function(e) {
+        var target = $(this).attr('href');
+        if (target[0] === "#") {
+            e.preventDefault();
+            if (target.length === 1 && typeof($(this).attr('data-scroll-up')) !== 'undefined') {
+                $body
+                    .velocity("scroll", {
+                        duration: 1000,
+                        easing: "swing"
+                    });
+            } else {
+                $(target)
+                    .velocity("scroll", {
+                        duration: 1000,
+                        easing: "swing"
+                    });
+            }
+        }
+    });
+
+    //=================================================================
+    // Fastclick
+    //=================================================================
+    FastClick.attach(document.body);
+
+});
