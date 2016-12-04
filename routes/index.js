89f9d3632bb9e5e12c6a27fbc421e89e2a56c7d9
Michal Drbohlav <m.drbohlav1@gmail.com>
2016-12-04 17:19:50 +0100

    hotfix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var routes = require('express').Router();
 var schools = require('./schools'),
     school_types = require('./school_types'),
     school_regions = require('./school_regions'),
-    school_specializations = require('./school_specializations'),
     tasks = require('./tasks'),
     task_types = require('./task_types'),
     users = require('./users');
@@ -23,7 +22,6 @@ routes.get('/submit', function(req, res, next) {
 routes.use('/schools', schools);
 routes.use('/school-types', school_types);
 routes.use('/school-regions', school_regions);
-routes.use('/school-specializations', school_specializations);
 routes.use('/tasks', tasks);
 routes.use('/task-types', task_types);
 routes.use('/users', users);
