0cde671e1bde1a4793ea35932b8b70dd49863545
Michal Drbohlav <m.drbohlav1@gmail.com>
2016-12-04 12:08:15 +0100

    updated DB connections

--- knexfile.js
+++ knexfile.js
@@ -21,33 +21,14 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'pg',
-    connection: {
-        host     : process.env.DB_HOST || dbCredentials.host,
-        port     : process.env.DB_PORT || dbCredentials.port,
-        user     : process.env.DB_USER || dbCredentials.user,
-        password : process.env.DB_PASSWORD || dbCredentials.password,
-        database : process.env.DB_NAME || dbCredentials.database,
-        ssl: true
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
     connection: {
-        host     : process.env.DB_HOST || dbCredentials.host,
-        port     : process.env.DB_PORT || dbCredentials.port,
-        user     : process.env.DB_USER || dbCredentials.user,
-        password : process.env.DB_PASSWORD || dbCredentials.password,
-        database : process.env.DB_NAME || dbCredentials.database,
+        host     : process.env.DB_HOST,
+        port     : process.env.DB_PORT,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_NAME,
         ssl: true
     },
     pool: {
